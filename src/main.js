0ec096c379414daca5d62cb590344bb816e93c95
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2018-08-13 05:19:36 -0700

    Moved more code into core.js

--- src/main.js
+++ src/main.js
@@ -5,59 +5,7 @@
 // http://github.com/workhorsy/ExampleWebGL
 
 
-let gl = null;
-let prev_ticks = 0;
-let program = null;
-let g_position_location = null;
-let g_texcoord_location = null;
-let g_matrix_location = null;
-let g_texture_location = null;
-let g_vertex_buffer = null;
-let g_texcoord_buffer = null;
 let g_sprites = [];
-
-
-
-function init() {
-	// Get the canvas
-	const canvas = document.querySelector("#canvas");
-	if (! canvas) {
-		throw new Error(`Failed to find the canvas.`);
-	}
-
-	// Get the canvas WebGL context
-	gl = canvas.getContext("webgl");
-	if (! gl) {
-		throw new Error(`Failed to get canvas WebGL context.`);
-	}
-
-	// Set black as the clear screen color
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-	// Clear the screen
-	gl.clear(gl.COLOR_BUFFER_BIT);
-
-	// Create the program
-	program = createProgram(gl, ["#vertex_shader", "#fragment_shader"]);
-
-	// Get the attributes
-	g_position_location = gl.getAttribLocation(program, "a_position");
-	g_texcoord_location = gl.getAttribLocation(program, "a_texcoord");
-
-	// Get the uniforms
-	g_matrix_location = gl.getUniformLocation(program, "u_matrix");
-	g_texture_location = gl.getUniformLocation(program, "u_texture");
-
-	// Create a vertex buffer that contains a quad
-	g_vertex_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, g_vertex_buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, DEFAULT_QUAD.slice(), gl.STATIC_DRAW);
-
-	// Create a texture buffer that contains a quad
-	g_texcoord_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, g_texcoord_buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, DEFAULT_QUAD.slice(), gl.STATIC_DRAW);
-}
 
 function onLogic(ticks) {
 	for (let sprite of g_sprites) {
